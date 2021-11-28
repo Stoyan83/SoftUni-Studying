969c6f82acdaefdee3ba632d6b3a92dc0dfb6f94
Stoyan83 <95132005+Stoyan83@users.noreply.github.com>
2021-11-28 02:48:24 -0800

    Create 08_pet_shop.py

---
+++ First Steps in Coding/Lab/08_pet_shop.py
@@ -0,0 +1,8 @@
+dog_food_packages = int(input())
+cat_food_packages = int(input())
+dog_food_price = 2.5
+cat_food_price = 4
+
+total_sum = dog_food_packages * dog_food_price + cat_food_packages * cat_food_price
+
+print(f"{total_sum} lv.")

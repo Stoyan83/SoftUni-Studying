3aacdda99d838a11dc138fa33adaf08c8336411a
Stoyan83 <95132005+Stoyan83@users.noreply.github.com>
2021-11-28 03:43:59 -0800

    Create 07_house_painting.py

---
+++ First Steps in Coding/More Exercises/07_house_painting.py
@@ -0,0 +1,17 @@
+x = float(input())
+y = float(input())
+h = float(input())
+
+front_wall = x * x - 1.2 * 2
+rear_wall = x * x
+left_wall = x * y - 1.5 * 1.5
+right_wall = x * y - 1.5 * 1.5
+front_roof = x * h/2
+side_roof = x * y
+
+green_paint = (front_wall + rear_wall + left_wall + right_wall) / 3.4
+red_paint = (front_roof * 2 + side_roof * 2) / 4.3
+
+
+print(f'{green_paint:.2f}')
+print(f'{red_paint:.2f}')

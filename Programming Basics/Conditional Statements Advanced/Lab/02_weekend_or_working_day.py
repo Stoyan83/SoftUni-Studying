93502b575be865757acb393d7221ead362b6d84d
Stoyan Stoyanov <stoyan_stoyanov@rocketmail.com>
2022-03-28 08:53:31 +0300

    some old solutions

---
+++ Programming Basics/Conditional Statements Advanced/Lab/02_weekend_or_working_day.py
@@ -0,0 +1,26 @@
+day = input()
+
+if day == "Saturday":
+    print("Weekend")
+
+elif day == "Sunday":
+    print("Weekend")
+
+elif day == "Monday":
+    print("Working day")
+
+elif day == "Tuesday":
+    print("Working day")
+
+elif day == "Wednesday":
+    print("Working day")
+
+elif day == "Thursday":
+    print("Working day")
+
+elif day == "Friday":
+    print("Working day")
+
+
+else:
+    print("Error")

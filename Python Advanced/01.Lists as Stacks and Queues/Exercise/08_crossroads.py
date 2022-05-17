cec2122c3e127e52bf46ec9555d34a5c86d44a80
Stoyan Stoyanov <95132005+Stoyan83@users.noreply.github.com>
2022-05-17 14:01:01 +0300

    Update 08_crossroads.py

--- Python Advanced/01.Lists as Stacks and Queues/Exercise/08_crossroads.py
+++ Python Advanced/01.Lists as Stacks and Queues/Exercise/08_crossroads.py
@@ -1,42 +1,35 @@
 from collections import deque
 
-green_light = int(input())
-yellow_light = int(input())
+green_window = int(input())
+free_window = int(input())
 
-counter = 0
-time_to_quit = False
 cars = deque()
+passed_cars = 0
+has_crashed = False
 
-while not time_to_quit:
-    while True:
-        car = input()
-        if car == "END":
-            print(f"Everyone is safe.")
-            print(f"{counter} total cars passed the crossroads.")
-            time_to_quit = True
-            break
-        if car == "green":
-            green = green_light
-            yellow = yellow_light
-            break
-        cars.append(car)
-    while green != 0:
-        if not cars:
-            break
-        passing_car = cars.popleft()
-        passing_car_parts = deque(passing_car)
-        while passing_car_parts:
-            if time_to_quit:
+while True:
+    line = input()
+    if line == "END":
+        break
+
+    if line == "green":
+        current_green = green_window
+        while cars and current_green > 0:
+            car = cars.popleft()
+            if current_green >= len(car) or current_green + free_window >= len(car):
+                passed_cars += 1
+                current_green -= len(car)
+            else:
+                print('A crash happened!')
+                print(f'{car} was hit at {car[current_green + free_window]}.')
+                has_crashed = True
                 break
-            char = passing_car_parts.popleft()
-            green -= 1
-            if green == 0 and passing_car_parts:
-                while passing_car_parts:
-                    char = passing_car_parts.popleft()
-                    yellow -= 1
-                    if yellow == -1:
-                        print(f"A crash happened!")
-                        print(f"{passing_car} was hit at {char}.")
-                        time_to_quit = True
-                        break
-        counter += 1
+    else:
+        cars.append(line)
+
+    if has_crashed:
+        break
+
+if not has_crashed:
+    print('Everyone is safe.')
+    print(f'{passed_cars} total cars passed the crossroads.')
